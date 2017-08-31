5d50c1ea3bc969c97c43ea8755362ba8304d1d4c
qiru <bjqiru@corp.netease.com>
2017-08-31 19:58:47 +0800

    添加应用

--- public/scripts/pages/simple.js
+++ public/scripts/pages/simple.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 
-let example = new Vue({
+var example = new Vue({
    el: '#example1',
    data: {
       message: 'Hello Vue!'
+   }
+})
+
+var app2 = new Vue({
+   el: '#app-2',
+   data: {
+      message: '页面加载于 ' + new Date().toLocaleString()
    }
 })
