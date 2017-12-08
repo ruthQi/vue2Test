4a0a53a0145678fd150018e0c87a4f4458945eb3
ruthQi <qi0428ru@163.com>
2017-12-08 18:42:49 +0800

    Update requestAdapter.js

--- public/scripts/utils/requestAdapter.js
+++ public/scripts/utils/requestAdapter.js
@@ -3,20 +3,7 @@
 
 import uri from 'utils/uri';
 
-/**
- *
- * @param params
- *  {
- *   "method":"get", ///get post delete请求方法
- *    "url": "https://live.ent.163.com/api/userInfo",//请求地址,也可以/api/userInfo
- *    "data": {      //请求参数
- *       a : 1
- *    }
- *  }
- *
- */
 
-//
 let get = (input, data = {}, json = true, init = {}) => {
 
    return uri.get(input, data, json, init);
